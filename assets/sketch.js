43c209eda9a991f8e57d49d5122592d9e747dfd3
callnasty <andre@grrlz.net>
2022-02-07 19:49:42 +0100

    some quick updates

--- assets/sketch.js
+++ assets/sketch.js
@@ -1,24 +1,34 @@
 function setup() {
-    var canvasDiv = document.getElementById('p5canvas');
-    var width = canvasDiv.clientWidth;
-    var height = canvasDiv.scrollHeight;
-    var canvas = createCanvas(width,(height - 100));
-    canvas.style('position', 'absolute');
-    canvas.style('top', '0');
-    canvas.style('right', '0');
-    canvas.style('z-index', '-1');
-    canvas.parent("p5canvas");
+  var canvasDiv = document.getElementById("p5canvas");
+  var width = canvasDiv.clientWidth;
+  var height = canvasDiv.scrollHeight;
+  var canvas = createCanvas(width, height - 100);
+  canvas.style("position", "absolute");
+  canvas.style("top", "0");
+  canvas.style("left", "0");
+  canvas.style("width", "100%");
+  canvas.style("height", "100%");
+  canvas.style("z-index", "-1");
+  canvas.parent("p5canvas");
+  frameRate(16);
 }
 
+let pointX;
+let pointY;
+let morph = 0;
+let pointCol;
+
 function draw() {
-    // background(14, 05, 250);
-    var index = 0;
-    while (index <= 20) {
-        circle(30+(index*10), 30+(index*10), 20);
-        index++;
-    }
+  morph = random(0,1)
+//  pointCol = lerpColor('#3FBEF0', '#CED639', morph);
+  pointCol = lerpColor(color(63,190,240), color(206,214,57), morph);
+  pointX = random(0, displayWidth);
+  pointY = random(0, displayHeight);
+  fill(pointCol);
+  noStroke();
+  ellipse(pointX, pointY, 10, 10);
 }
-  
-  function windowResized() {
-    resizeCanvas(width,height);
-  }
+
+function windowResized() {
+  resizeCanvas(width, height);
+}
