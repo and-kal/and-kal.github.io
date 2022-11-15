bbc9a2fbd05183a25c0d23693f3c1b32fe236ea2
and-kal <andre@grrlz.net>
2022-11-15 10:46:14 +0100

    first working version

--- .eleventy.js
+++ .eleventy.js
@@ -5,10 +5,8 @@ module.exports = function(eleventyConfig) {
       return `
         <a ${fileslug==="" ? "class='active'" : ""} href="/">Home</a>
         <a ${fileslug==="livecoding" ? "class='active'" : ""} href="/livecoding">Livecoding</a>
-        <a ${fileslug==="dj" ? "class='active'" : ""} href="/dj">DJing</a>
-        <a ${fileslug==="bands" ? "class='active'" : ""} href="/music">music</a>
+        <a ${fileslug==="bands" ? "class='active'" : ""} href="/music">Music</a>
         <a ${fileslug==="imprint" ? "class='active'" : ""} href="/imprint">Imprint</a>
-        <a href="/blog" target="_blank" rel="noopener noreferrer">Blog</a>
       `; 
     });
     return {
