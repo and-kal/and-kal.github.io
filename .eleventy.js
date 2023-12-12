eab97ebe35db709644277138e20aea5ac685d750
and-kal <andre@grrlz.net>
2023-12-12 13:53:57 +0100

    some small updates here and there

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,32 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.ignores.add("README.md");
-    eleventyConfig.ignores.add(".gitignore");
-    
-    eleventyConfig.addPassthroughCopy('./*.css');
-    eleventyConfig.addPassthroughCopy('./assets');
-    eleventyConfig.addLiquidShortcode("header-active", function(fileslug) { 
-      return `
-        <a ${fileslug==="" ? "class='active'" : ""} href="/">Home</a>
-        <!-- <a ${fileslug==="blog" ? "class='active'" : ""} href="/blog">Blog</a> -->
-        <a ${fileslug==="livecoding" ? "class='active'" : ""} href="/livecoding">Algorave</a>
-        <a ${fileslug==="music" ? "class='active'" : ""} href="/music">Music</a>
-        <a ${fileslug==="webdev" ? "class='active'" : ""} href="/webdev">Web Dev</a>
-        <a ${fileslug==="imprint" ? "class='active'" : ""} id="imprint" href="/imprint">Imprint</a>
-      `; 
-    });
-    return {
-      passthroughFileCopy: true
-    };
+module.exports = function (eleventyConfig) {
+  eleventyConfig.ignores.add("README.md");
+  eleventyConfig.ignores.add(".gitignore");
+
+  eleventyConfig.addPassthroughCopy("./*.css");
+  eleventyConfig.addPassthroughCopy("./assets");
+  eleventyConfig.addLiquidShortcode("header-active", function (fileslug) {
+    return `
+        <a ${
+          fileslug === "" || fileslug === "/now" ? "class='active'" : ""
+        } href="/">Now</a>
+        <!-- <a ${
+          fileslug === "blog" ? "class='active'" : ""
+        } href="/blog">Blog</a> -->
+        <a ${
+          fileslug === "livecoding" ? "class='active'" : ""
+        } href="/livecoding">Algorave</a>
+        <a ${
+          fileslug === "music" ? "class='active'" : ""
+        } href="/music">Music</a>
+        <a ${
+          fileslug === "webdev" ? "class='active'" : ""
+        } href="/webdev">Web Dev</a>
+        <a ${
+          fileslug === "imprint" ? "class='active'" : ""
+        } id="imprint" href="/imprint">Imprint</a>
+      `;
+  });
+  return {
+    passthroughFileCopy: true,
   };
+};
