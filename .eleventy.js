137e00b787c6e0d97657a3f08097c99659726403
and-kal <andre@grrlz.net>
2024-08-16 15:30:46 +0200

    advance shortcodes for 'now' page

--- .eleventy.js
+++ .eleventy.js
@@ -26,6 +26,46 @@ module.exports = function (eleventyConfig) {
         } id="imprint" href="/imprint">Imprint</a>
       `;
   });
+  eleventyConfig.addLiquidShortcode("letterboxd", function () {
+    const convert = require("xml-js");
+    /* get latest items via RSS */
+    const RSS_URL = `https://letterboxd.com/staxl/rss`;
+
+    fetch(RSS_URL)
+      .then((response) => response.text())
+      .then((str) => {
+        dataAsJson = JSON.parse(convert.xml2json(str));
+        console.log(dataAsJson);
+      });
+    // .then((data) => `<div>${data}</div>`);
+  });
+  eleventyConfig.addLiquidShortcode("bookwyrm", function () {
+    const convert = require("xml-js");
+    /* get latest items via RSS */
+    const RSS_URL = `https://bookwyrm.social/user/staxl/rss`;
+
+    fetch(RSS_URL)
+      .then((response) => response.text())
+      .then((str) => {
+        dataAsJson = JSON.parse(convert.xml2json(str));
+        const latestActivityElements =
+          dataAsJson.elements[0].elements[0].elements.reduce((memo, elem) => {
+            if (elem.name === "item") {
+              const html = elem.elements.reduce((a, b) => {
+                if (b.name === "description") {
+                  a = b.elements[0].text;
+                }
+                return a;
+              });
+              memo.push(`<p>${html}</p>`);
+            }
+            // get the latest 5 elements
+            return memo.slice(0, 5);
+          }, []);
+        return latestActivityElements;
+      });
+  });
+
   return {
     passthroughFileCopy: true,
   };
