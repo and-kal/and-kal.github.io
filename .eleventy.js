b026ad5f7ae1690c783abdbbffd41c2ed61ba23b
and-kal <andre@grrlz.net>
2024-08-20 14:27:29 +0200

    first working version of now page

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,21 @@
+function getRSSContent(dataAsJson) {
+  return dataAsJson.elements[0].elements[0].elements.reduce((memo, elem) => {
+    if (elem.name === "item") {
+      const html = elem.elements.reduce((a, b) => {
+        if (b.name === "description") {
+          // TODO: add date for bookwyrm fetch
+          a = b.elements[0].text || b.elements[0].cdata;
+        }
+        return a;
+      });
+      const cleanedHtml = html.split("staxl ")[1] || html;
+      memo.push(`<div>${cleanedHtml}</div>`);
+    }
+    // get the latest 5 elements
+    return memo.slice(0, 5);
+  }, []);
+}
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.ignores.add("README.md");
   eleventyConfig.ignores.add(".gitignore");
@@ -7,8 +25,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addLiquidShortcode("header-active", function (fileslug) {
     return `
         <a ${
+          fileslug === "" || fileslug === "/" ? "class='active'" : ""
+        } href="/">Home</a>
+        <a ${
           fileslug === "" || fileslug === "/now" ? "class='active'" : ""
-        } href="/">Now</a>
+        } href="/now">Now</a>
         <!-- <a ${
           fileslug === "blog" ? "class='active'" : ""
         } href="/blog">Blog</a> -->
@@ -26,44 +47,33 @@ module.exports = function (eleventyConfig) {
         } id="imprint" href="/imprint">Imprint</a>
       `;
   });
-  eleventyConfig.addLiquidShortcode("letterboxd", function () {
+  eleventyConfig.addLiquidShortcode("letterboxd", async function () {
     const convert = require("xml-js");
     /* get latest items via RSS */
     const RSS_URL = `https://letterboxd.com/staxl/rss`;
 
-    fetch(RSS_URL)
+    const latest = await fetch(RSS_URL)
       .then((response) => response.text())
       .then((str) => {
         dataAsJson = JSON.parse(convert.xml2json(str));
-        console.log(dataAsJson);
+        const latestActivityElements = getRSSContent(dataAsJson);
+        return latestActivityElements;
       });
-    // .then((data) => `<div>${data}</div>`);
+    return latest;
   });
-  eleventyConfig.addLiquidShortcode("bookwyrm", function () {
+  eleventyConfig.addLiquidShortcode("bookwyrm", async function () {
     const convert = require("xml-js");
     /* get latest items via RSS */
     const RSS_URL = `https://bookwyrm.social/user/staxl/rss`;
 
-    fetch(RSS_URL)
+    const latest = await fetch(RSS_URL)
       .then((response) => response.text())
       .then((str) => {
         dataAsJson = JSON.parse(convert.xml2json(str));
-        const latestActivityElements =
-          dataAsJson.elements[0].elements[0].elements.reduce((memo, elem) => {
-            if (elem.name === "item") {
-              const html = elem.elements.reduce((a, b) => {
-                if (b.name === "description") {
-                  a = b.elements[0].text;
-                }
-                return a;
-              });
-              memo.push(`<p>${html}</p>`);
-            }
-            // get the latest 5 elements
-            return memo.slice(0, 5);
-          }, []);
+        const latestActivityElements = getRSSContent(dataAsJson);
         return latestActivityElements;
       });
+    return latest;
   });
 
   return {
